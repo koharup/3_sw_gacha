a1d9caf187d02681590c30dbc9b482634b02e44d
koharup <anandpita18@icloud.com>
2020-03-24 14:54:00 +0900

    Initial Commit

---
+++ 3_sw_gacha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3_sw_gacha
+//
+//  Created by kohari on 2020/03/24.
+//  Copyright © 2020 kohari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
